0c2d930f55e518dce832451cf5886f8dab8316f8
asendia2025 <aseng@ase-semi.com>
2026-01-07 18:19:54 +0900

    Add files via upload

---
+++ core_project_data.js
@@ -0,0 +1,30 @@
+/**
+ * Core Project Data Structure
+ * tasks : 프로젝트명
+ * dept : 책임부서
+ * category : RFG, RFM, Controller, EVC
+ * model: 대상제품모델
+ * target : 최종프로젝트 종료 목표
+ * phase_0_plan / close : 기획단계 일정
+ * phase_1_plan / close : 제품개발 및 검증평가 일정
+ * JDP_plan / close : 장비사평가 일정
+ * Status : 현재 상태 (On Track, Delayed, Critical, Completed)
+ * Note : 이슈사항
+ */
+window.coreProjectData = [
+    {
+        "tasks": "LDMOS Alternative Development",
+        "dept": "R&D",
+        "category": "RFG",
+        "model": "Not Determined",
+        "target": "2026-07-30",
+        "phase_0_plan": "2026-01-15",
+        "phase_0_close": "",
+        "phase_1_plan": "2026-02-28",
+        "phase_1_close": "",
+        "JDP_plan": "2026-07-30",
+        "JDP_close": "",
+        "status": "On Track",
+        "note": "진행사항을 파악해야 합니다."
+    }
+];
